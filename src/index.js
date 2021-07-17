d06553809461ab2bcda31ba747af352d3b6bfe7c
Laisvunas <saugos@gmail.com>
2021-07-17 16:21:48 +0300

    init commit2

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send({msg: "Server is running successfully"}); 
+});
+
+app.use("/auth", authRoutes);
+
+app.all("*", (req, res) => {
+    res.status(404).send({ error: "Page not found" });
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
