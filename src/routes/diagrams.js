f434b4f849b509869c174ce67f74f4a62ba56a41
Laisvunas <saugos@gmail.com>
2021-07-21 09:21:42 +0300

    diagrams/publish route added

--- src/routes/diagrams.js
+++ src/routes/diagrams.js
@@ -5,6 +5,7 @@ const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 const {mysqlConfig, jwtSecret} = require("../config");
+const { isLoggedIn } = require('../middleware/middleware');
 
 router.post("/all", async (req, res) => {
     if (!req.body.author_id || !req.body.isEditor ) {
@@ -25,6 +26,42 @@ router.post("/all", async (req, res) => {
         con.end();
 
         return res.send(returnObj);
+    }
+    catch(e) {
+        console.log(e);
+        res.status(500).send({ error: "DB error"});
+    }
+});
+
+router.post("/publish", isLoggedIn, async (req, res) => {
+    if (!req.body.author_id || !req.body.title || !req.body.sentence || !req.body.code) {
+        return res.status(400).send({error: "Insufficient data provided"});
+    }
+
+    try {
+        const con = await mysql.createConnection(mysqlConfig);
+
+        // case Publish
+        if (!req.body.id) {
+            const [data] = await con.execute(`INSERT INTO diagrams (user_id, title, sentence, code, commentary ${req.body.editor == 'y' ? ', editors_commentary' : ''}) VALUES (${mysql.escape(req.body.author_id)}, ${mysql.escape(req.body.title)}, ${mysql.escape(req.body.sentence)}, ${mysql.escape(req.body.code)}, ${mysql.escape(req.body.commentary)}) ${req.body.editor == 'y' ? mysql.escape(req.body.editors_commentary) : ''}`);
+            
+            if (data.affectedRows !== 1) {
+                return res.status(500).send({error: "Error in DB"});
+            }
+
+            return res.send({data, msg: "The diagram has been successfully saved."});
+        }
+        // case Edit
+        else {
+            const [data] = await con.execute(`UPDATE diagrams SET title = ${mysql.escape(req.body.title)}, sentence = ${mysql.escape(req.body.sentence)}, code = ${mysql.escape(req.body.code)}, commentary = ${mysql.escape(req.body.commentary)} ${req.body.editor == 'y' ? ', editors_commentary = ' + mysql.escape(req.body.editors_commentary) : ''} WHERE id = ${mysql.escape(req.body.id)}`);
+
+            console.log("data: " + JSON.stringify(data));
+
+            return res.send({data, msg: "The diagram has been successfully updated."});
+        }
+
+        con.end();
+
     }
     catch(e) {
         console.log(e);
