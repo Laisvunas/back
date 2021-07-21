943c22dcdd6a9e49f89c1e395bd27bf757016b9e
Laisvunas <saugos@gmail.com>
2021-07-21 09:22:08 +0300

    middleware added

---
+++ src/middleware/middleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const { jwtSecret } = require("../config");
+
+module.exports = {
+    isLoggedIn(req, res, next) {
+        try {
+            const token = typeof req.headers.authorization != 'undefined' ? req.headers.authorization.split(" ")[1] : 'undefined';
+            //const token = req.headers.authorization?.split(' ')[1];
+            const decodedToken = jwt.verify(token, jwtSecret);
+            req.user_from_token = decodedToken;
+            if (req.body.author_id === decodedToken.id) {
+                return next();
+            }
+            
+        } catch (e) {
+            return res.status(400).send({ error: 'Unauthorized access to site' });
+        }
+    },
+};
